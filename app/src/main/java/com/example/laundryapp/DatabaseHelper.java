b0d5e8a749eb098bfa5b55dc633888a991cee514
Natasya <natasyacaca916@gmail.com>
2026-04-18 10:20:08 +0800

    ya

--- app/src/main/java/com/example/laundryapp/DatabaseHelper.java
+++ app/src/main/java/com/example/laundryapp/DatabaseHelper.java
@@ -12,6 +12,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "laundry.db";
     private static final int DATABASE_VERSION = 2;
 
+
     public static final String TABLE_USERS = "users";
     public static final String COL_USER_ID = "id";
     public static final String COL_USER_NAME = "name";
